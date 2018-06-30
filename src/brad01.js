2e1b6bfdcc6e676d5c42bf795a8677defb94482d
admin <brad@brad.tw>
2018-06-30 16:22:19 +0800

    2018063005

--- src/brad01.js
+++ src/brad01.js
@@ -23,19 +23,19 @@ var Brad01Layer = cc.Layer.extend({
         });
         this.addChild(this.status);
 
-        this.poker7 = new cc.Sprite(
-            res.poker_png,cc.rect(221,711,220,218));
-        this.poker7.attr({
-            x: cc.winSize.width / 4,
-            y: cc.winSize.height /2,
-            rotation: -90,
-        });
-        this.addChild(this.poker7);
+        // this.poker7 = new cc.Sprite(
+        //     res.poker_png,cc.rect(221,711,220,218));
+        // this.poker7.attr({
+        //     x: cc.winSize.width / 4,
+        //     y: cc.winSize.height /2,
+        //     rotation: -90,
+        // });
+        // this.addChild(this.poker7);
 
         cc.spriteFrameCache.addSpriteFrames(
             res.poker_plist, res.poker_png);
 
-        this.poker4 = new cc.Sprite("#pokers_s04.png");
+        this.poker4 = new cc.Sprite("#pokers_0.png");
         this.poker4.attr({
             x: cc.winSize.width *3/ 4,
             y: cc.winSize.height /2,
